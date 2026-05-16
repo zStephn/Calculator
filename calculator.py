a3773cf0da2ebabd8f43cf9402db3670628f611d
zStephn <steven49091@gmail.com>
2026-05-16 18:29:27 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
-op = [num1+num2,num1-num2,num1*num2,num1/num2]
 
-print(op[3])
+op = [num1+num2, num1-num2, num1*num2, num1/num2, num1%num2, num1**num2]
+
+print(op[5])
